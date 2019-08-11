69572841a2e9283261cd09ca0cc9c2c8e4ca0a01
Peter <peter.hurt@aol.de>
2019-08-11 14:30:15 +0200

    CMAKE: Added cmake environment. This makes it easier to build.

---
+++ build/CMakeFiles/aerial_emergency_helper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aerial_emergency_helper.dir/main.cpp.o"
+  "aerial_emergency_helper.pdb"
+  "aerial_emergency_helper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aerial_emergency_helper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <sys/stat.h>
 
 using namespace std;
@@ -11,11 +12,13 @@ using namespace cv;
     1. Helpers should not be in danger as well
     2. Affected people can get ill / need food and medicine,... 
     3. ...
+
+    Dependencies: -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imcodecs
 */
 
 // Global Variables
 string HOME_PATH = getenv("HOME");
-string IMAGE_PATH = HOME_PATH + "/" + "Pictures" + "/" + "aerial_footage" + "/" + "erdrutsch.jpg";
+string IMAGE_PATH = HOME_PATH + "/" + "Pictures" + "/" + "aerial_footage";
 
 /**
  * pathExist(const std::string) returns true if the given path exists and false if the given path does not exist 
@@ -37,11 +40,28 @@ int main()
     if ( !pathExist(IMAGE_PATH) ) return 1;
     cout << "Loading aerial images from " + IMAGE_PATH << endl;
 
-    cv::String path(IMAGE_PATH);
-    //glob(IMAGE_PATH, fn, false); // get all filenames in directory
+    vector<cv::String> fn;
+    glob(IMAGE_PATH, fn, false); // get all filenames in directory
 
-    //Mat image;
-    //image = imread(IMAGE_PATH, CV_LOAD_IMAGE_COLOR);
+    vector<Mat> image_data;
+    vector<String> image_names;
+    for (size_t k=0; k<fn.size(); ++k) // loop through all images
+    {
+        Mat image = imread(fn[k], CV_LOAD_IMAGE_COLOR);
+
+        if (image.empty()) continue; 
+        
+        // store names and images
+        image_names.push_back(fn[k]);
+        image_data.push_back(image);
+    }
+
+    namedWindow( image_names[0], WINDOW_NORMAL );// Create a window for display.
+    
+    imshow( image_names[0], image_data[0] );                   // Show our image inside it.
+    resizeWindow(image_names[0], 600, 600); // resize window
+
+    waitKey(0);
 
     // ---------------------------------------- //
     // edge extraction -> will only work with clear edges
