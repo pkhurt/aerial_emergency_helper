24e046687608f036648b3f9427d58b3f72985bd0
Peter <peter.hurt@aol.de>
2019-08-11 10:39:47 +0200

    INITIAL COMMIT: New features will be implemented from here. Yet, only the body and description is implemented.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+/*
+    The task: After natural disasters or hazards there are often problems finding emergency exits to the people that need help. Especially bringing goods over landway and help people to escape is one of the main difficult tasks.
+    1. Helpers should not be in danger as well
+    2. Affected people can get ill / need food and medicine,... 
+    3. ...
+*/
+
+// Global Variables
+
+
+main()
+{
+    // load aerial image
+    cout << "Loading aerial images..." << endl;
+
+    // edge extraction -> will only work with clear edges
+    cout << "Extracting edges..." << endl;
+
+    // Extract coordinates
+    cout << "Extracting coordinates..." << endl;
+}
