9b725693b8898dc73b2ac6062e173303f6a9ab32
Peter <peter.hurt@aol.de>
2019-08-11 15:01:30 +0200

    NEW FEATURE: Edges are now extracted. However, further processing is needed to extract streets and ways properly.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 using namespace std;
 using namespace cv;
@@ -20,6 +21,16 @@ using namespace cv;
 string HOME_PATH = getenv("HOME");
 string IMAGE_PATH = HOME_PATH + "/" + "Pictures" + "/" + "aerial_footage";
 
+Mat src_image, src_image_gray;
+Mat dst_image, detected_edges;
+
+int edgeThresh = 1;
+int lowThreshold;
+int const max_lowThreshold = 100;
+int ratio = 3;
+int kernel_size = 3;
+char* window_name = "Edge Map";
+
 /**
  * pathExist(const std::string) returns true if the given path exists and false if the given path does not exist 
  * 
@@ -31,6 +42,27 @@ bool pathExist(const std::string &s)
   struct stat buffer;
   return (stat (s.c_str(), &buffer) == 0);
 }
+
+/**
+ * @function CannyThreshold
+ * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
+ */
+void CannyThreshold(int, void*)
+{
+  /// Reduce noise with a kernel 3x3
+  blur( src_image_gray, detected_edges, Size(3,3) );
+
+  /// Canny detector
+  Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+
+  /// Using Canny's output as a mask, we display our result
+  dst_image = Scalar::all(0);
+
+  src_image.copyTo(dst_image, detected_edges);
+  imshow( window_name, dst_image );
+  resizeWindow(window_name, 600, 600); // resize window
+
+ }
 
 int main()
 {
@@ -45,27 +77,45 @@ int main()
 
     vector<Mat> image_data;
     vector<String> image_names;
+
     for (size_t k=0; k<fn.size(); ++k) // loop through all images
     {
-        Mat image = imread(fn[k], CV_LOAD_IMAGE_COLOR);
+        src_image = imread(fn[k], CV_LOAD_IMAGE_COLOR);
 
-        if (image.empty()) continue; 
+        if (src_image.empty()) continue; 
         
         // store names and images
         image_names.push_back(fn[k]);
-        image_data.push_back(image);
+        image_data.push_back(src_image);
     }
 
-    namedWindow( image_names[0], WINDOW_NORMAL );// Create a window for display.
-    
-    imshow( image_names[0], image_data[0] );                   // Show our image inside it.
-    resizeWindow(image_names[0], 600, 600); // resize window
-
-    waitKey(0);
+    cout << "Found " << image_names.size() << " image(s) for processing." << endl;
 
     // ---------------------------------------- //
     // edge extraction -> will only work with clear edges
     cout << "Extracting edges..." << endl;
+    // process all found images
+    for (size_t k=0; k<image_names.size(); ++k)
+    {
+        cout << "Processing image " << image_names[k] << endl;
+
+        // USING CANNY EDGE DETECTOR OF OPENCV
+        //create a matrix of the same type and size as src image
+        dst_image.create(src_image.size(), src_image.type());
+
+        // Convert image to grayscale
+        cvtColor(src_image, src_image_gray, CV_BGR2GRAY);
+
+        namedWindow( window_name, WINDOW_NORMAL );// Create a window for display.
+    
+        //imshow( image_names[k], image_data[k] );  // Show our image inside it.
+
+        createTrackbar("Min threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
+
+        CannyThreshold(0, 0);
+
+        waitKey(0);
+    }
 
     // ---------------------------------------- //
     // Extract coordinates
